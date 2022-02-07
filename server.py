73beba89ddac93cb067e8ae793dd098008257984
CarloshLameira <carloshlameira@hotmail.com>
2022-02-07 19:27:20 -0300

    Chat done

--- server.py
+++ server.py
@@ -1,18 +1,37 @@
 import socket
-HOST = 'localhost'
+import threading
+
+HOST = '127.0.0.1'
 PORT = 8001
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
 
-sock.bind((HOST, PORT))
+chats = {}
 
-sock.listen(5)
+def broadcast(room, mensage):
+    for i in chats[room]:
+        if isinstance(mensage, str):
+            mensage = mensage.encode()
 
-chats =  {''}
+        i.send(mensage)
+
+def enviarMensagem(name, room, client):
+    while True:
+        mensage = client.recv(1024)
+        mensage = f"{name}: {mensage.decode()}\n"
+        broadcast(room, mensage)
 
 while True:
-        novoSock, _ = sock.accept()
-        sala = novoSock.recv(1024).decode()
-        
-        novoSock.send(b'ok')
-    
+    client, addr = server.accept()
+    client.send(b'SALA')
+    room = client.recv(1024).decode()
+    name = client.recv(1024).decode()
+    if room not in chats.keys():
+        chats[room] = []
+    chats[room].append(client)
+    print(f'{name} se conectou na sala {room}! INFO {addr}')
+    broadcast(room, f'{name}: Entrou na sala!\n')
+    thread = threading.Thread(target=enviarMensagem, args=(name, room, client))
+    thread.start()
